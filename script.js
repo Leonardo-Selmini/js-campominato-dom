3184f6cb44078d446809036a67814ae3d4ea664d
Leonardo.Selmini <selminileonardo@gmail.com>
2021-11-19 01:16:42 +0100

    add CSS + bonus

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ let gameState = {
 }
 
 document.getElementById('play').addEventListener('click', createGame)
-// 00000000 FUNZIONI 00000000
-
-function createGame(){
-  gameState.counter = 0;
+function createGame() {
+  
   document.getElementById('banner').classList.remove('visible')
 
   let difficulty = document.getElementById('difficulty').value;
   let gameClass = 'width-7';
-  switch(difficulty) {
+
+  switch (difficulty) {
     case 'easy':
       gameClass = 'width-7';
       gameState.squares = 49;
@@ -21,48 +20,48 @@ function createGame(){
       break;
     case 'medium':
       gameClass = 'width-10';
-      gameState.squares = 100
-      gameState.bombs = 20
+      gameState.squares = 100;
+      gameState.bombs = 20;
       break;
     case 'hard':
       gameClass = 'width-14';
-      gameState.squares = 196
-      gameState.bombs = 60
+      gameState.squares = 196;
+      gameState.bombs = 60;
       break;
   }
-
   spawnSquare(gameState.squares, gameClass);
   spawnBombs(gameState.bombs, gameState.squares);
 }
 
 function updateScore(isBomb) {
-  let banner = document.getElementById('banner')
-  let result = document.getElementById('result')
-  let resultCounter = document.getElementById('result-counter')
+  let banner = document.getElementById('banner');
+  let result = document.getElementById('result');
+  let resultCounter = document.getElementById('result-counter');
 
-  if(isBomb) {
+  if (isBomb) {
     //hai perso
-    banner.classList.add('visible')
-    result.innerHTML = 'Hai perso'
-    resultCounter.innerHTML = gameState.counter
+    banner.classList.add('visible');
+    result.innerHTML = 'Hai perso';
+    resultCounter.innerHTML = gameState.counter;
+    gameState.counter = 0;
   } else {
     gameState.counter++;
   }
 
-  if(gameState.counter == gameState.squares - gameState.bombs){
+  if (gameState.counter == gameState.squares - gameState.bombs) {
     //hai vinto
-    banner.classList.add('visible')
-    result.innerHTML = 'Hai vinto'
-    resultCounter.innerHTML = gameState.counter
+    banner.classList.add('visible');
+    result.innerHTML = 'Hai vinto';
+    resultCounter.innerHTML = gameState.counter;
+    gameState.counter = 0;
   }
 
   document.getElementById('counter').innerHTML = gameState.counter;
-  document.getElementById('restart-btn').addEventListener('click', function(){
+  document.getElementById('restart-btn').addEventListener('click', function () {
     createGame()
   })
 }
 
-// spawnSquare serve per generare n (segnaposto nSquare) quadrati di lunghezza x (segnaposto classeWidth)
 function spawnSquare(nSquare, classeWidth) {
   // svuoto la griglia
   document.getElementById('grid').innerHTML = '';
@@ -72,11 +71,10 @@ function spawnSquare(nSquare, classeWidth) {
     // do classe square di base + segnaposto classeWidth
     div.classList.add('square', classeWidth);
     // aggiungo funzionalità con il click
-    div.addEventListener('click', function() {
-      if (!div.classList.contains('active')){
+    div.addEventListener('click', function () {
+      if (!div.classList.contains('active')) {
         div.classList.add('active');
         updateScore(false)
-        checkAdiacentSquares()
       }
     })
     // inserisco il quadrato nella griglia
@@ -84,12 +82,10 @@ function spawnSquare(nSquare, classeWidth) {
   }
 }
 
-// nRandom serve per generare un numero casuale tra min e max che devono essere due numeri
 function nRandom(max, min) {
   Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// spawnBombs serve per generare un numero(nBomb) di bombe e inserirle casualmente tra i x(nSquare) quadrati
 function spawnBombs(nBomb, nSquare) {
   let squares = document.getElementsByClassName('square');
   let bombs = [];
@@ -98,7 +94,7 @@ function spawnBombs(nBomb, nSquare) {
     if (bombs.includes(bomb) == false) {
       bombs.push(bomb);
       squares[bomb - 1].innerHTML = `<i class="fas fa-bomb"></i>`
-      squares[bomb - 1].addEventListener('click', function (){
+      squares[bomb - 1].addEventListener('click', function () {
         squares[bomb - 1].classList.add('bg-bomb')
         updateScore(true);
         showBombs(squares, bombs);
@@ -107,9 +103,9 @@ function spawnBombs(nBomb, nSquare) {
   }
 }
 
-function showBombs(squares, bombs){
-  for(let i = 0; i < bombs.length; i++){
-    squares[bombs[i]-1].classList.add('bg-bomb')
+function showBombs(squares, bombs) {
+  for (let i = 0; i < bombs.length; i++) {
+    squares[bombs[i] - 1].classList.add('bg-bomb')
   }
 }
 
